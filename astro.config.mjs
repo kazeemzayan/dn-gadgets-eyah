57be2301512c2dd0911d6d2cdadb81de1a927cf8
kazeemzayan <146915615+kazeemzayan@users.noreply.github.com>
2023-10-04 14:52:07 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercelServerless from '@astrojs/vercel/serverless';
- 
+import vercel from '@astrojs/vercel/serverless';
+
 export default defineConfig({
   output: 'server',
-  adapter: vercelServerless(),
+  adapter: vercel(),
 });
